54e7cd0013cc9260c306c22dc935b2d1c2e58a6b
Kyle Nicholson <kyleanicholson@gmail.com>
2024-01-14 19:59:26 -0800

    Create workout functionality + supabase integration

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Create from "../views/Create.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register,
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: Create,
   },
 ];
 
